cac964d731ef1f5b1300d0aa58af58bca63ee7f0
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-26 18:30:05 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,8 @@
-# module Findable
+module Findable
   
-#   def find_by_name(name)
-#     self.all.detect{|a| a.name == name}
-#   end
+  def find_by_name(name)
+    self.all.detect{|a| a.name == name}
+  end
   
-# end 
+end 
 
