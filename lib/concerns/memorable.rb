369fab8cd14889e069e6eaf791fa50ac69ae6cf0
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-26 18:49:41 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,14 @@
 module Memorable 
   
-  def reset_all
-    self.all.clear
-  end
-
-  def count
-    self.all.count
-  end
+  module ClassMethods
+    
+    def reset_all
+      self.all.clear
+    end
+  
+    def count
+      self.all.count
+    end
+  
   
 end 
